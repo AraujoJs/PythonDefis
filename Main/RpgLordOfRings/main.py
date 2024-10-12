14b15078d37431fa7b930a1135fcbdd77120cdab
ARAUJO José <jose.araujo-firmino-de-lima@etu.univ-grenoble-alpes.fr>
2024-10-12 16:32:55 +0200

    Création du RPG Seigneur Des Anneaux. Ajout du fichier main.py et création des fonctions de base pour création d'une partie.

---
+++ Main/RpgLordOfRings/main.py
@@ -0,0 +1,42 @@
+#coding: UTF-8
+"" "
+Script:Main / RpgLordOfRings / main
+Cr � ���ation:jojo, le 12 / 10 / 2024
+"" "
+#Imports
+import logging, menu
+
+
+#Configurations globales
+logging.getLogger().setLevel(logging.DEBUG)
+
+#Fonctions
+
+
+
+#Programme principal
+
+def start_game(game):
+games =[range(5)]
+
+def new_game(name):
+#TODO: créer nouveau game
+pass
+
+def get_list_game_choice()->list:
+#TODO: retourner la liste des games sauvegardés
+pass
+
+def get_list_game()->list:
+return[]
+
+
+def main():
+menu.show_menu_principal()
+menu.get_menu_choice()
+pass
+
+
+
+if __name__== '__main__':
+	main()
