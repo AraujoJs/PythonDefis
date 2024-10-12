2dab8d2e54ff74234c55dcc961e431f56740870a
ARAUJO José <jose.araujo-firmino-de-lima@etu.univ-grenoble-alpes.fr>
2024-10-12 16:33:45 +0200

    Ajout du fichier saves.py pour la logique d'enregistrement de game.

---
+++ Main/RpgLordOfRings/saves.py
@@ -0,0 +1,14 @@
+# coding: UTF-8
+"""
+Script: PenduGame/saves.py
+Création: jojo, le 12/10/2024
+"""
+# Imports
+import logging
+
+# Configurations globales
+logging.getLogger().setLevel(logging.DEBUG)
+
+
+# Fonctions
+
